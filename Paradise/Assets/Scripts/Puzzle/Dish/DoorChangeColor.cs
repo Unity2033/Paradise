4fcf4c7fa0ce96819bd012ed86f12018e435fe13
lim7635 <ju7635@naver.com>
2024-08-20 20:51:26 +0900

    2024.08.20 DoorChangeColor 스크립트 수정

--- Paradise/Assets/Scripts/Puzzle/Dish/DoorChangeColor.cs
+++ Paradise/Assets/Scripts/Puzzle/Dish/DoorChangeColor.cs
@@ -1,43 +1,36 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class DoorChangeColor : MonoBehaviour
 {
-    [SerializeField] List<Color> colors = new List<Color>();
-    [SerializeField] Material material;
+    [SerializeField] List<Color> colors;
 
     WaitForSeconds delay;
     WaitForSeconds totalDelay;
 
     void Start()
     {
-        colors.Add(Color.yellow);   
-        colors.Add(Color.red);
-        colors.Add(new Color(0f, 97f / 255, 255f / 255));  //ÇÏ´Ã
-        colors.Add(new Color(255f / 255, 0, 206f / 255)); // ºÐÈ«
-        colors.Add(Color.black);   
-        colors.Add(new Color(0, 31f / 255, 93f / 255));   // ³²»ö
-        colors.Add(Color.white);
-        colors.Add(Color.green);
-        colors.Add(new Color(255f / 255, 117f / 255, 0)); // ÁÖÈ²
-
         StartCoroutine(ChangeColor());
     }
 
     IEnumerator ChangeColor()
     {
         delay = new WaitForSeconds(0.5f);
-        totalDelay = new WaitForSeconds(1.5f);
+        totalDelay = new WaitForSeconds(2f);
 
-        while(true)
+        while (true)
         {
-            for(int i = 0; i < colors.Count; i++)
+            for (int i = 0; i < colors.Count; i++)
             {
-                yield return delay;
+                gameObject.GetComponent<Renderer>().material.color = colors[i];
 
-                material.color = colors[i];
+                yield return delay;
             }
+
+            gameObject.GetComponent<Renderer>().material.color = Color.clear;
 
             yield return totalDelay;
         }
